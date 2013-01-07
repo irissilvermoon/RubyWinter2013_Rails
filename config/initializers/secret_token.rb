63019263a885d421717884e9456aae7362daa949
Karen Caplan <keaslk@gmail.com>
2013-01-07 15:19:21 -0800

    first commit - bare bones rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = '9b91446013d93f21fe463aede68a9e8e17ad2f6228dbf7ac36e55c5e66c6c6598ab17649acbb7251c1352d022d49d31d88fda255a58ba80ac4a4f047c5321acd'
